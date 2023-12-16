55857e1278005e457d717664527a3cf7df338230
9897664 <olgadzyu224@mail.ru>
2023-12-16 17:09:52 +0400

    Create first spring project

---
+++ src/main/java/ru/dziuban/firstspringproject/service/TranslateService.java
@@ -0,0 +1,20 @@
+package ru.dziuban.firstspringproject.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TranslateService {
+
+    public String translateDayOfWeek(String name) {
+        return switch (name) {
+            case "MONDAY" -> "Понедельник";
+            case "TUESDAY" -> "Вторник";
+            case "WEDNESDAY" -> "Среда";
+            case "THURSDAY" -> "Четверг";
+            case "FRIDAY" -> "Пятница";
+            case "SATURDAY" -> "Суббота";
+            case "SUNDAY" -> "Воскресенье";
+            default -> throw new IllegalStateException("Unexpected value: " + name);
+        };
+    }
+}
